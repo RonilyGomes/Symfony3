c49650ded91cf0ce4201001e87c766894e614b3f
Ronily <ronily.web@gmail.com>
2017-08-30 23:38:56 -0300

    symfony com Doctrine

---
+++ src/doctrine.php
@@ -0,0 +1,27 @@
+<?php
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$paths = [
+    __DIR__ . '/Entity'
+];
+
+$isDevMode = true;
+
+$dbParams = [
+    'driver'    => 'pdo_mysql',
+    'user'      => 'root',
+    'password'  => 'AnHfDUIy8qOqP6hT',
+    'dbname'   => 'doctrine_basico'
+];
+
+$config = Setup::createAnnotationMetadataConfiguration($paths,$isDevMode);
+
+$entityManager = EntityManager::create($dbParams,$config);
+
+function getEntityManager()
+{
+    global $entityManager;
+    return $entityManager;
+}
